b6dac941af5e046922b8761a6fea1c0d85df5449
Ken Woon <xk.woon@gmail.com>
2025-01-28 02:42:32 -0800

    feat(navbar): implement navigation menu and modify index

---
+++ src/assets/index.js
@@ -0,0 +1,11 @@
+import logo from './logo-black-nobg.png'
+import menu from './menu.svg'
+import close from './close.svg'
+import menu_bg from './menu-bg.jpg'
+
+export {
+  logo,
+  menu,
+  close,
+  menu_bg,
+} 
---
+++ src/constants/index.js
@@ -0,0 +1,49 @@
+export const navLinks = [
+    {
+        id: "",
+        title: "TOP"
+    },
+    {
+        id: "#about",
+        title: "ABOUT"
+    },
+    {
+        id: "#services",
+        title: "SERVICES"
+    },
+    {
+        id: "#projects",
+        title: "PROJECTS"
+    }
+];
+
+export const navLinksSmall = [
+    {
+        id: "#contact",
+        title: "Contact"
+    }
+]
+
+export const projects = [
+    // Below is an example, replace with actual projeccts later
+    {
+        name: "AI-Powered Analytics",
+        description: "Advanced analytics platform leveraging machine learning for real-time data visualization and predictive insights, helping businesses make data-driven decisions.",
+        tags: [
+            {
+                name: "react",
+                color: "text-lumia-blue",
+            },
+            {
+                name: "tensorflow",
+                color: "text-lumia-purple",
+            },
+            {
+                name: "python",
+                color: "text-lumia-blue",
+            },
+        ],
+        image: "/project/analytics.png",
+        source_code_link: "https://github.com/",
+    }
+]; 
